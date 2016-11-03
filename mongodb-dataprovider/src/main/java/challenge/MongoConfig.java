4ecf187c95a8a56390717a5f765f27a81e2bed14
Carlos B <carlos.baezruiz@gmail.com>
2016-11-03 21:45:36 +0100

    Delete boot dependencies and added variables to keep same dependencies

--- mongodb-dataprovider/src/main/java/challenge/MongoConfig.java
+++ mongodb-dataprovider/src/main/java/challenge/MongoConfig.java
@@ -1,9 +1,10 @@
 package challenge;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -13,28 +14,19 @@ import com.mongodb.WriteConcern;
 
 @EnableMongoRepositories("challenge")
 @Configuration
-@EnableConfigurationProperties
-@ConfigurationProperties(locations = "classpath:application.properties", ignoreUnknownFields = true, prefix = "spring.data.mongodb")
+@PropertySource(value = "classpath:application.properties")
 public class MongoConfig extends AbstractMongoConfiguration {
 
-	private String database;
-	private String host;
+	@Autowired
+	Environment env;
 
 	public MongoConfig() {
 	}
 
 	@Override
-	protected String getDatabaseName() {
-		return database;
-	}
-
-	public void setDatabaseName(final String databaseName) {
-		this.database = databaseName;
-	}
-
-	@Override
 	@Bean
 	public MongoClient mongo() throws Exception {
+		String host = env.getProperty("spring.data.mongodb.host");
 		final MongoClient client = new MongoClient(host);
 		client.setWriteConcern(WriteConcern.SAFE);
 		return client;
@@ -42,12 +34,19 @@ public class MongoConfig extends AbstractMongoConfiguration {
 
 	@Bean
 	public MongoTemplate mongoTemplate() throws Exception {
+		String database = env.getProperty("spring.data.mongodb.databaseName");
+		String host = env.getProperty("spring.data.mongodb.host");
 		return new MongoTemplate(new MongoClient(host), database);
 	}
 
 	@Override
 	public String toString() {
 		return "[MongoConfig]";
+	}
+
+	@Override
+	protected String getDatabaseName() {
+		return env.getProperty("spring.data.mongodb.databaseName");
 	}
 
 }
